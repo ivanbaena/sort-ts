730d71a974414b907f1006bb4716c5aa23fda3ce
Ivan Baena <baenadev.io@gmail.com>
2020-07-26 00:23:23 -0400

    remove character sort

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,29 +1,22 @@
-import { NumbersCollection } from './NumbersCollection';
-
 interface Sortable {
   length: number;
   compare(leftIndex: number, rightIndex: number): boolean;
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {
-    this.collection = collection;
-  }
+export abstract class Sorter {
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
   }
 }
-
-const numbersCollection = new NumbersCollection([3, 0, -15, -10]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-import './Sorter';
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+const numbersCollection = new NumbersCollection([3, 0, -15, -10]);
+numbersCollection.sort();
+console.log(numbersCollection);
+
+const charactersCollection = new CharactersCollection('xaabbyre');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(-500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+
+linkedList.print();
